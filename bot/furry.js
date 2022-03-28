ea944f45ee66b139ec9b377ba5de9bdf1cd2502d
davisbruce413 <davisbruce413@gmail.com>
2022-03-28 08:00:53 -0500

    fixed .env

--- bot/furry.js
+++ bot/furry.js
@@ -1,7 +1,21 @@
 const translate = require("./furry/translator");
+const axios = require('axios');
 
 let furry = {};
 
 furry.translate = translate;
+
+furry.getBibles = () => {
+
+axios
+  .get('https://example.com/todos')
+  .then(res => {
+    console.log(`statusCode: ${res.status}`)
+    console.log(res)
+  })
+  .catch(error => {
+    console.error(error)
+  })
+}
 
 module.exports = furry;
